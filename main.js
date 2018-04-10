d92dcfc650135789580a8e1266dac15a795061c0
ervinbani <bani.ervin@gmail.com>
2018-04-10 14:52:09 +0200

    ----

--- main.js
+++ main.js
@@ -2,9 +2,21 @@ $(document).ready(function(){
   var messaggio=$('#btn-right');
   var input=$('#msgwriter');
   messaggio.click(function(){
-    $('.greentext').text(input.val());
-    $('.whitetext').text("Ok", 5000);
+        $('.greentext').text(input.val());
+
+        $('.whitetext').text("Ok", (5000));
 
   });
+  var x=$('#contact-img').attr("src")
+  console.log(x);
+  var contact=$('.contact');
+  contact.click(function(){
+    $('#friends-img').attr("src",$('#contact-image').attr("src"));
+    $('#Contatto').text($('#contact-name').text());
+
+  })
+
+
+
 
 });
