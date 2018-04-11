98a3a9d95e7b323ae77263fa3c7f91191daeec0f
ervinbani <bani.ervin@gmail.com>
2018-04-11 14:37:50 +0200

    -----

--- main.js
+++ main.js
@@ -1,25 +1,55 @@
 $(document).ready(function(){
 
 
-  var i=0;
 
     var cont=$('.contact');
   cont.click(function(){
-    $('.friends-img').removeAttr("src");
     $('.friends-img').attr("src",$('.contact-image').attr("src"));
+
     $('.Contatto').text($('.contact-name').text());
-    i++;
+
 
   });
   var messaggio=$('#btn-right');
   var input=$('#msgwriter');
-  messaggio.click(function(){
+      messaggio.click(function(){
         $('.right-container').append("<div class='greentext'>"
-      + input.val() + "</div>");
-     $('.right-container').append("<div class='whitetext'>"
-    + "Ok" + "</div>");
+        + input.val() + "</div>"+"<div class='clear'>"+ "</div>");
+
+      $('.right-container').append("<div class='whitetext'>"
+         + "Ok" + "</div>");
+      input.val("");
 
 
 
   });
+  var clearDiv = '<div class="clear"></div>',
+messageTagOpen = '<div class="greentext">',
+messageTagClose = '</div>',
+spanTimeOpen = '<span class="message-time">',
+spanTimeClose = '</span>';
+  $('#msgwriter').keypress(function(e) {
+		if (e.which == 13) {
+			var thisInput = $(this),
+			newMessage = thisInput.val(),
+			now = new Date();
+
+			if (newMessage) {
+				//now i can append to the messages
+				var footerCnt = thisInput.parent(),
+				rightContainer = footerCnt.parent()
+				messagesContainer = rightContainer.children('.right-messages');
+
+				  $('.right-container').append(
+					messageTagOpen + newMessage + spanTimeOpen + now.getHours() + ':' + now.getMinutes() + spanTimeClose + messageTagClose + clearDiv
+				);
+        $('.right-container').append("<div class='whitetext'>"
+           + "Ok" + "</div>");
+
+				thisInput.val('');
+
+				messagesContainer.scrollTop(messagesContainer[0].scrollHeight)
+			}
+		}
+	});
   });
